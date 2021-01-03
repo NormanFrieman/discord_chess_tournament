f143ff65c8644d5ffda3b603bc32047f634a8a43
NormanFrieman <ezequiel-d.s@hotmail.com>
2021-01-03 00:18:16 -0300

    new update

--- src/discord/commands/HelloWorld.ts
+++ src/discord/commands/HelloWorld.ts
@@ -1,4 +1,9 @@
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+
 function HelloWorld(message: any): void{
+    const config = require("../../../config.json");
+    const hook = new Webhook(`${config.WEBHOOK}`);
+
     const prefix = "!";
 
     if(message.author.bot) return;
@@ -7,9 +12,15 @@ function HelloWorld(message: any): void{
     const command = message.content.slice(prefix.length).toLowerCase();
 
     if(command == "hello"){
-        const timeTaken = Date.now() - message.createdTimestamp;
-
-        message.reply(`Hello World! This message had a latency of ${timeTaken}ms...`);
+        const embed = new MessageBuilder()
+            .setTitle('Hello World!')
+            .addField('First field', 'this is inline', true)
+            .addField('Second field', 'this is not inline')
+            .setColor('#00b0f4')
+            .setDescription('Oh look a description :weary:');
+        
+        hook.send(embed);
+//        message.reply(`Hello World! This message had a latency of ${timeTaken}ms...`);
     }
 };
 
